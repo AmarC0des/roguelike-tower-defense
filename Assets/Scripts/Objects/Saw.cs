e12ab6814c06eec53db75f6296a06f869da0808e
Ryan T <memopemomemo@gmail.com>
2025-04-24 20:50:29 -0400

    Revert "Merge branch 'EnemyControllerANdAssets' of https://github.com/AmarC0des/roguelike-tower-defense into EnemyControllerANdAssets"
    This reverts commit 848d00e91ff08e4bc078e42730a59dbcf4243053, reversing
    changes made to ba9365e580cdf41b4d7d389eb078fa7bd7f94075.

--- Assets/Scripts/Objects/Saw.cs
+++ Assets/Scripts/Objects/Saw.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/Objects/Saw.cs
 public class Saw : MonoBehaviour
-========
-public class Ability : MonoBehaviour
->>>>>>>> 7a3047806b949e0f86a2b8901bbcff6f70939a55:Assets/Scripts/DataStructs/Ability.cs
 {
     // Start is called before the first frame update
     void Start()
@@ -18,10 +14,5 @@ public class Ability : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void Activate()
-    {
-
     }
 }
