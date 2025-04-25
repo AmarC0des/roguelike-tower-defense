cfa66886086ad69c10ad1f11a8fd05c2811d71e4
Ryan T <memopemomemo@gmail.com>
2025-04-24 20:51:15 -0400

    Revert "Enemy Ai, Garrett Terrain, Eric UI"
    This reverts commit ba9365e580cdf41b4d7d389eb078fa7bd7f94075.

--- Assets/Scripts/DataStructs/Abilites.cs
+++ Assets/Scripts/DataStructs/Abilites.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability : MonoBehaviour
+public class Abilites : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,10 +14,5 @@ public class Ability : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void Activate()
-    {
-
     }
 }
--- Assets/Scripts/PathManager.cs
+++ Assets/Scripts/PathManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.UI;
-using Cinemachine;
+
 public class PathManager : MonoBehaviour
 {
 
